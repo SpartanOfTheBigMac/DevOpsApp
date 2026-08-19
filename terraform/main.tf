81c2e6038901bb3c98230135f779f12d95dfe290
Ethan Franklin <ethan@Ethans-MacBook-Pro.local>
2026-08-19 19:22:52 +0100

    try agian

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,6 +36,10 @@ resource "aws_instance" "web_github" {
   key_name        = "vockey"
   security_groups = [aws_security_group.web_github.name]
 
+  tags = {
+    Name = "academy-web_github"
+  }
+
   user_data = <<-EOF
               #!/bin/bash
 
@@ -59,10 +63,7 @@ resource "aws_instance" "web_github" {
               EOF
 }
 
-  tags = {
-    Name = "academy-web_github"
-  }
-}
+
 
 output "ec2_public_ip" {
   value = aws_instance.web_github.public_ip
