a0229713ae29ac74c347396b463f9848fdf9e5a1
Ethan Franklin <ethan@Ethans-MacBook-Pro.local>
2026-08-19 19:37:53 +0100

    why has this hurt me so much

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,10 +60,10 @@ resource "aws_instance" "web_github13" {
               EOF
 
   tags = {
-    Name = "academy-web_github"
+    Name = "academy-web_github13"
   }
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.web_github.public_ip
+  value = aws_instance.web_github13.public_ip
 }
