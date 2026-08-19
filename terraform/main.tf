c8176900a1901dbcf2567372893a359a77068d35
Ethan Franklin <ethan@Ethans-MacBook-Pro.local>
2026-08-19 18:54:40 +0100

    I am crying

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "web_github" {
 
 resource "aws_instance" "web_github" {
   ami           = "ami-0e86e20dae9224db8"
-  instance_type = var.instance_type
+  instance_type = t2.micro
   key_name      = "vockey"
   security_groups = [aws_security_group.web_github.name]
 
